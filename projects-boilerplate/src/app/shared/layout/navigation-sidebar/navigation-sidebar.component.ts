e4bc2c628bd1b4794fed5e18f3ef6172cb5d3706
Matthieu Playe <matthieu.playe@student.howest.be>
2022-07-13 19:46:19 +0200

    Add base navigation-sidebar component

---
+++ projects-boilerplate/src/app/shared/layout/navigation-sidebar/navigation-sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-sidebar',
+  templateUrl: './navigation-sidebar.component.html',
+  styleUrls: ['./navigation-sidebar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavigationSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
